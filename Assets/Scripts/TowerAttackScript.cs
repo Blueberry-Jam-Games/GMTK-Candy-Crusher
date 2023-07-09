9c78411fa43e8ddc6e94d4b49180a878c9e3c939
Kaiden Richardson <kaidenr@shaw.ca>
2023-07-09 07:11:58 -0700

    Tutorial repair.

--- Assets/Scripts/TowerAttackScript.cs
+++ Assets/Scripts/TowerAttackScript.cs
@@ -41,9 +41,6 @@ public class TowerAttackScript : BlockingObject
     [SerializeField]
     public int ammo;
 
-    [SerializeField]
-    public int ammo;
-
     Dictionary<int, bool> playerTracking;
 
     private void Start()
